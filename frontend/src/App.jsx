68f681c36336fb33dc3e2ae3c588628db247e017
Nikhil Sharma <nikhilsharma0437@gmail.com>
2025-07-12 15:13:27 +0530

    User Dashboard in dark theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Authentication from './pages/Authentication'
 import AddItemForm from './pages/AddItemForm'
+import ReWearUserDashboard from './pages/RewearUserDashboard'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element={<Navigate to='/auth' replace />} />
           <Route path='/auth' element={<Authentication />} />
           <Route path='/list' element={<AddItemForm />} />
+          <Route path='/UserDashboard' element={<ReWearUserDashboard />} /> 
         </Routes>
       </Router>
     </>
